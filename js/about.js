b2881bde6f92a288887eb1bdd21d5e52bf615d3b
lennyaaaa <112637527+lennyaaaa@users.noreply.github.com>
2023-05-18 23:26:13 +0200

    added hiding button for text and made buttons for topics about me

--- js/about.js
+++ js/about.js
@@ -1,3 +1,11 @@
-console.log("about.js loaded");
-
 let aboutText = document.querySelector(".aboutText");
+let btnContinue = document.querySelector(".btnContinue");
+let buttonTopic = document.querySelector(".buttonTopic");
+
+btnContinue.addEventListener("click", hideFunction);
+
+function hideFunction() {
+    aboutText.classList.add("hide");
+    console.log("btnContinue clicked");
+    buttonTopic.classList.remove("hide");
+};
