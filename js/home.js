63c78ae5bbd8befaa99e611eb787223612186edf
lennyaaaa <112637527+lennyaaaa@users.noreply.github.com>
2023-05-18 15:13:39 +0200

    added new bg to home and text

--- js/home.js
+++ js/home.js
@@ -1,21 +1,23 @@
 console.log('home.js loaded');
 
-const EL_img = document.querySelector("#img");
-const pos = {x:0, y:0};
+// const EL_img = document.querySelector("#img");
+// const pos = {x:0, y:0};
 
-document.addEventListener("keydown", (ev) => {
+// document.addEventListener("keydown", (ev) => {
 
-  const dir = (ev.key.match(/(?<=^Arrow)\w+/)||[])[0];
-  if (!dir) return; // Not an arrow key.
+//   const dir = (ev.key.match(/(?<=^Arrow)\w+/)||[])[0];
+//   if (!dir) return; // Not an arrow key.
   
-  ev.preventDefault(); // Prevent Browser scroll if overflow
+//   ev.preventDefault(); // Prevent Browser scroll if overflow
 
-  ({
-    Left:  () => pos.x -= 10,
-    Right: () => pos.x += 10,
-    Up:    () => pos.y -= 10,
-    Down:  () => pos.y += 10,
-  }[dir])(); 
+//   ({
+//     Left:  () => pos.x -= 10,
+//     Right: () => pos.x += 10,
+//     Up:    () => pos.y -= 10,
+//     Down:  () => pos.y += 10,
+//   }[dir])(); 
   
-  EL_img.style.transform = `translate(${pos.x}px, ${pos.y}px)`
-});
+//   EL_img.style.transform = `translate(${pos.x}px, ${pos.y}px)`
+// });
+
+// to move an img
