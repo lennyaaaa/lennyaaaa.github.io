dc53cc36a2208c2843b7aae1f45303a99639f767
lennyaaaa <112637527+lennyaaaa@users.noreply.github.com>
2023-06-02 12:59:34 +0200

    reworked website

--- js/main.js
+++ js/main.js
@@ -1,2 +1,18 @@
-console.log("main.js loaded");
+console.log('Hello World');
 
+const sectionHome = document.querySelector('.home-section').style.display = "none";
+const sectionAbout = document.querySelector('.about-section');
+const navHome = document.querySelector('.nav-home');
+const navAbout = document.querySelector('.nav-about');
+const navContact = document.querySelector('.nav-contact');
+const navProjects = document.querySelectorAll('.nav-projects');
+
+
+navHome.addEventListener('click', () => {
+    sectionAbout.classList.add('d-none');
+    sectionHome.classList.remove('d-none');
+});
+navAbout.addEventListener('click', () => {
+    sectionHome.classList.add('d-none');
+    sectionAbout.classList.remove('d-none');
+});
