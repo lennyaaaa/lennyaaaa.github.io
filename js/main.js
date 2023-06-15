5fec564ee8426d4c655610e4a58d948ec352dd08
lennyaaaa <112637527+lennyaaaa@users.noreply.github.com>
2023-06-15 12:15:19 +0200

    added some pages

--- js/main.js
+++ js/main.js
@@ -1,18 +1,7 @@
 console.log('Hello World');
 
-const sectionHome = document.querySelector('.home-section').style.display = "none";
-const sectionAbout = document.querySelector('.about-section');
-const navHome = document.querySelector('.nav-home');
-const navAbout = document.querySelector('.nav-about');
-const navContact = document.querySelector('.nav-contact');
-const navProjects = document.querySelectorAll('.nav-projects');
+const aboutNav = document.querySelector('.aboutNav')
 
-
-navHome.addEventListener('click', () => {
-    sectionAbout.classList.add('d-none');
-    sectionHome.classList.remove('d-none');
-});
-navAbout.addEventListener('click', () => {
-    sectionHome.classList.add('d-none');
-    sectionAbout.classList.remove('d-none');
-});
+aboutNav.addEventListener('click', function() {
+    window.location.href = '../pages/about.html';
+})
