68d6903a4ccf99599cd63ca7c8b58ab79059aab4
lennyaaaa <112637527+lennyaaaa@users.noreply.github.com>
2024-04-08 21:27:45 +0200

    Port Linux

--- js/main.js
+++
@@ -1,7 +0,0 @@
-console.log('Hello World');
-
-const aboutNav = document.querySelector('.aboutNav')
-
-aboutNav.addEventListener('click', function() {
-    window.location.href = '../pages/about.html';
-})
---
+++ script.js
@@ -0,0 +1,65 @@
+function handleInput(event) {
+    if (event.keyCode === 13) { // Check if the Enter key is pressed
+        var input = document.getElementById("username").value.trim();
+        if (input !== "") {
+            clearTerminal();
+            displayWelcomeMessage(input);
+            displayTypeBelowMessage();
+        }
+    }
+}
+
+function clearTerminal() {
+    document.getElementById("terminalBody").innerHTML = "";
+}
+
+function displayWelcomeMessage(username) {
+    var welcomeMessage = document.createElement("div");
+    welcomeMessage.innerHTML = "<div class='command-output'>Welcome, " + username + "!</div>" +
+                                "<div class='command-output'>What would you like to see? Contact, Projects, About Me</div>";
+    document.getElementById("terminalBody").appendChild(welcomeMessage);
+}
+
+function displayTypeBelowMessage() {
+    var typeMessage = document.createElement("div");
+    typeMessage.innerHTML = "<div class='command-output'>Type below: </div>";
+    document.getElementById("terminalBody").appendChild(typeMessage);
+
+    var inputField = document.createElement("input");
+    inputField.type = "text";
+    inputField.id = "userInput";
+    inputField.onkeydown = handleCategoryInput;
+    document.getElementById("terminalBody").appendChild(inputField);
+}
+
+function handleCategoryInput(event) {
+    if (event.keyCode === 13) { // Check if the Enter key is pressed
+        var category = document.getElementById("userInput").value.trim();
+        if (category !== "") {
+            handleCategory(category.toLowerCase());
+        }
+    }
+}
+
+function handleCategory(category) {
+    switch (category) {
+        case "contact":
+            window.location.href = "contact.html";
+            break;
+        case "projects":
+            window.location.href = "projects.html";
+            break;
+        case "about me":
+            window.location.href = "about.html";
+            break;
+        default:
+            displayErrorMessage(category);
+            break;
+    }
+}
+
+function displayErrorMessage(input) {
+    var errorMessage = document.createElement("div");
+    errorMessage.innerHTML = "<div class='command-output'>No '" + input + "' category found. Please try again.</div>";
+    document.getElementById("terminalBody").appendChild(errorMessage);
+}
